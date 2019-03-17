d2d4460ebc7dc3fc0fed5f34ec9714477b3e7ab0
raymondmiracle <34572535+raymondmiracle@users.noreply.github.com>
2019-03-17 05:28:32 -0700

    1st cam compile error fix

--- drivers/misc/mediatek/imgsensor/src/mt6753/ov8865_mipi_raw/ov8865mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/mt6753/ov8865_mipi_raw/ov8865mipiraw_Sensor.c
@@ -40,11 +40,13 @@
 #include <linux/fs.h>
 #include <asm/atomic.h>
 //#include <asm/system.h>
-#include <linux/xlog.h>
+//#include <linux/xlog.h>
+#include "kd_camera_typedef.h"
+
 
 #include "kd_camera_hw.h"
 #include "kd_imgsensor.h"
-#include "kd_imgsensor_define.h" 
+#include "kd_imgsensor_define.h"
 #include "kd_imgsensor_errcode.h"
 
 #include "ov8865mipiraw_Sensor.h"
